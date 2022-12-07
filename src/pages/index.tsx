ee1b6730a17bd347d1f9fd13337b3fb2d9e0695d
Mogy1945 <hatano.shun@shanon.co.jp>
2022-12-07 12:47:15 +0900

    Improve: Linkboxをコンポーネント化して設置した。
    index.tsx内にベタ書きしていたLinkbox（＝各ページへのリンク）を
    全ページ共通で設置することになったため。
    Linkboxはコンポーネント化したのち、
    index.tsx内のべた書きを削除し、コンポーネントとして設置した。
    
    feature:#8

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
 import styles from '../styles/Home.module.scss'
 import Header from '@/components/Header/Header'
+import Linkbox from '@/components/Linkbox/Linkbox'
 
 export default function Home() {
   return (
@@ -19,79 +18,30 @@ export default function Home() {
         <p className={styles.comment}>ひとこと：ポートフォリオ、はじめました。</p>
 
         <div className={styles.linkContainer}>
-          <div className={styles.linkBox}>
-            <Link href=''>
-              <div className={styles.linkBoxContains}>
-                <div className={styles.linkBoxLeft}>
-                  <Image
-                    src='/img/Home/home_link_profile.png'
-                    alt='プロフィールページへのリンクです。'
-                    width={72}
-                    height={72}
-                  />
-                </div>
-                <div className={styles.linkBoxRight}>
-                  <h2>PROFILE</h2>
-                  <p>経歴とプロフィールを記載しています。</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-          <div className={styles.linkBox}>
-            <Link href=''>
-              <div className={styles.linkBoxContains}>
-                <div className={styles.linkBoxLeft}>
-                  <Image
-                    src='/img/Home/home_link_skill.png'
-                    alt='スキルセット一覧ページへのリンクです。'
-                    width={72}
-                    height={72}
-                  />
-                </div>
-                <div className={styles.linkBoxRight}>
-                  <h2>SKILL-SET</h2>
-                  <p>経歴とプロフィールを記載しています。</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-          <div className={styles.linkBox}>
-            <Link href=''>
-              <div className={styles.linkBoxContains}>
-                <div className={styles.linkBoxLeft}>
-                  <Image
-                    src='/img/Home/home_link_product.png'
-                    alt='プロダクト一覧ページへのリンクです。'
-                    width={72}
-                    height={72}
-                  />
-                </div>
-                <div className={styles.linkBoxRight}>
-                  <h2>Product</h2>
-                  <p>経歴とプロフィールを記載しています。</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-          <div className={styles.linkBox}>
-            <Link href=''>
-              <div className={styles.linkBoxContains}>
-                <div className={styles.linkBoxLeft}>
-                  <Image
-                    src='/img/Home/home_link_contact.png'
-                    alt='お問い合わせページへのリンクです。'
-                    width={72}
-                    height={72}
-                  />
-                </div>
-                <div className={styles.linkBoxRight}>
-                  <h2>Contact</h2>
-                  <p>経歴とプロフィールを記載しています。</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-
+          <Linkbox
+            href='/Profile'
+            srcUrl='/img/Home/home_link_profile.png'
+            name='PROFILE'
+            detail='経歴とプロフィールを記載しています。'
+          />
+          <Linkbox
+            href='/Skillset'
+            srcUrl='/img/Home/home_link_skill.png'
+            name='SKILL-SET'
+            detail='基準を元にスキルセットを掲載しています。'
+          />
+          <Linkbox
+            href='/Product'
+            srcUrl='/img/Home/home_link_product.png'
+            name='PRODUCT'
+            detail='個人開発を行ったプロダクトを掲載してます。'
+          />
+          <Linkbox
+            href='/Contact'
+            srcUrl='/img/Home/home_link_contact.png'
+            name='CONTACT'
+            detail='お問い合わせはこちらか、ヘッダーのSNSからお願いいたします。'
+          />
           {/* linkContainer */}
         </div>
         {/* container */}
