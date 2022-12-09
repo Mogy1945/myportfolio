a6cfbb2d7941b6b73a1350732e31066341661fb5
Mogy1945 <hatano.shun@shanon.co.jp>
2022-12-09 14:12:43 +0900

    Improve: microCMSからフェッチしたデータをもとにコンポーネントを整形した
    microCMSとつなぐことで、JSX構造を変更する必要があったため。
    
    feature:#9

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,3 +1,4 @@
+import type { InferGetStaticPropsType, NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import React from 'react'
@@ -5,8 +6,25 @@ import styles from '../styles/Product.module.scss'
 import Header from '@/components/Header/Header'
 import Linkbox from '@/components/Linkbox/Linkbox'
 import Products from '@/components/Product_products/Products'
+import { client } from '@/libs/client'
+import type { Product } from '@/types/product'
 
-const Profile: React.FC = () => {
+export const getStaticProps = async () => {
+  const product = await client.get({ endpoint: 'products_detail' })
+
+  return {
+    props: {
+      products: product.contents,
+    },
+  }
+}
+
+type Props = {
+  products: Product[]
+}
+
+const Profile: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ products }: Props) => {
+  console.log(products)
   return (
     <>
       <Head>
@@ -14,29 +32,24 @@ const Profile: React.FC = () => {
         <meta name='description' content='MogyのPortfolioサイトのプロダクト一覧ページです。' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Header></Header>
+      <Header />
       <main className={styles.container}>
         <h1>Product</h1>
 
         <p className={styles.comment}>個人開発のプロダクト一覧ページです。</p>
 
         <div className={styles.productContainer}>
-          <Products
-            linkUrl='https://mogyblog-zeta.vercel.app/'
-            srcUrl='/img/Product/product_link_blog.png'
-            name='Mogy-Blog'
-            date='2022/11'
-            detail='私のブログです。本格的に個人開発を実践しようと思い作成した初めてのプロダクトです。microCMSと繋げることで、投稿と同時に内容が反映されるようになっています。また、SSGを利用することで、スムーズなUXに仕上げています。'
-            skill='Next.js、SSR、SSG、SCSS、microCMS、Formspree、Vercel'
-          />
-          <Products
-            linkUrl='https://myportfolio-five-virid.vercel.app/'
-            srcUrl='/img/Product/product_link_portfolio.png'
-            name='Mogy-Portfolio'
-            date='2022/12'
-            detail='私のポートフォリオです。ブログに引き続き2番目のプロダクトです。TypeScriptを勉強し、プロダクトとして利用したかったため、TSで作成しました。お仕事を依頼したい方の目線で動線やデザインを作成しています。'
-            skill='TypeScript、Next.js、SCSS、Formspree、Vercel'
-          />
+          {products.map((product) => (
+            <Products
+              key={product.id}
+              linkUrl={`/product/${product.id}`}
+              srcUrl={product.sumbnail}
+              name={product.name}
+              date={product.date}
+              detail={product.explanation}
+              skill={product.skill}
+            />
+          ))}
         </div>
         {/* productContainer */}
       </main>
