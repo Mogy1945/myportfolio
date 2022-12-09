6457e78cb6f08a608ffbb43391be4553f0b82bc5
Mogy1945 <hatano.shun@shanon.co.jp>
2022-12-09 14:05:55 +0900

    Add: 型ファイルを設定するフォルダ、ファイルを作成した。
    microCMSで呼び出すデータの型を設定するため。
    
    feature:#9

---
+++ src/types/product.ts
@@ -0,0 +1,13 @@
+export type Product = {
+  id: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+  name: string
+  date: string
+  explanation: string
+  skill: string
+  link: string
+  sumbnail: string
+}
