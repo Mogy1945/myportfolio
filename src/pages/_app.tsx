be4226a27e911034b932ed3d2b358d7ab9b25d8f
Mogy1945 <hatano.shun@shanon.co.jp>
2022-12-12 17:57:28 +0900

    Add: GAタグとその処理を追加しました。
    アナリティクスを利用するため。
    
    feature:#10

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/reset.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { usePageView } from '@/hooks/usePageView'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  usePageView()
+
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
